1c401e05cd943e8b6f3c94ddd2afaf5142ccdc29
comdori <02145s1@gmail.com>
2023-09-07 03:03:12 +0900

    Feat 우승자 기능 추가
    - 묵찌빠로 넘어가기 위한 조건 충족
    - 우승자를 선별 하기 위해 사용자와 컴퓨터 추가

--- RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
@@ -2,50 +2,61 @@
 //  RockPaperScissors.swift
 //  RockPaperScissors
 //
-//  Created by Wonji Ha on 2023/09/06.
+//  Created by 지준용 on 2023/09/06.
 //
 
 import Foundation
 
 final class RockPaperScissors {
     
+    // MARK: - Enum
+    
+    enum Player : String {
+        case mac = "컴퓨터"
+        case user = "사용자"
+    }
+    
     enum Hand : String, CaseIterable {
-        case scissor = "1"
+        case scissors = "1"
         case rock = "2"
         case paper = "3"
     }
     
-    private let aiHand = Hand.allCases.randomElement()
+    static var winner : Player?
+
+    // MARK: - Start
     
     func start() {
-        
         while true {
-            print(aiHand?.rawValue)
+            print(GuideMessage.rpsChoiceMenu, terminator: "")
+            guard let userInput = readLine() else { return }
             
-            print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-            guard let inputUser = readLine() else { return }
-            
-            if inputUser == "0" {
-                print("게임 종료")
-                break
+            if userInput == "0" {
+                return print(GuideMessage.exit)
             }
             
-            guard let userHand = Hand(rawValue: inputUser) else {
-                print("잘못된 입력입니다. 다시 시도해주세요.")
+            guard let userHand = Hand(rawValue: userInput) else {
+                print(GuideMessage.inputError)
                 continue
             }
             
+            let aiHand = Hand.allCases.randomElement()
+            
             if userHand == aiHand {
-                print("비겼습니다!")
-            }
-            else if (userHand == .scissor && aiHand == .paper) || (userHand == .rock && aiHand == .scissor) || (userHand == .paper && aiHand == .rock) {
-                print("이겼습니다!")
-                print("[사용자 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ")
-            }
-            else {
-                print("졌습니다!")
-                break
+                print(GuideMessage.draw)
+            } else if (userHand == .scissors && aiHand == .paper) ||
+                      (userHand == .rock && aiHand == .scissors) ||
+                      (userHand == .paper && aiHand == .rock) {
+                RockPaperScissors.winner = .user
+                print(GuideMessage.win)
+                Mukjjippa().play()
+              
+            } else {
+                RockPaperScissors.winner = .mac
+                print(GuideMessage.lose)
+                Mukjjippa().play()
             }
         }
     }
 }
+
